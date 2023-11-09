d8e3df88c8fce54a0e628f84f2370ab4593f6c3e
ex-n <139383917+ex-n@users.noreply.github.com>
2023-11-09 12:13:02 -0500

    Update api.tf

--- api.tf
+++ api.tf
@@ -1,6 +1,10 @@
 resource "google_project_service" "project" {
+  for_each = toset([
+    "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
+  ])
   project = var.project
-  service = "compute.googleapis.com"
+  service = each.value
 
   disable_dependent_services = true
 }
